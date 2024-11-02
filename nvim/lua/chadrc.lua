dccd681ee4a2356c2c0f3a3fdb675683a175a538
shotoyaar <anirudhsridhar1064@gmail.com>
2024-11-02 11:34:19 +0530

    all dotfiles that are needed really

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,16 +1,73 @@
--- This file  needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-	theme = "yoru",
+M.base46 = {
+    theme = "catppuccin",
 
-	hl_override = {
-	 	Comment = { italic = true },
-	 	["@comment"] = { italic = true },
-	},
+    hl_override = {
+        Comment = { italic = true },
+        ["@comment"] = { italic = true },
+    },
+}
+
+M.ui = {
+    cmp = {
+        icons = true,
+        lspkind_text = true,
+    },
+
+    statusline = {
+        theme = "vscode_colored",
+        separator_style = "block",
+        enabled = true,
+    },
+
+    tabufline = {
+        order = { "treeOffset", "buffers", "tabs", "btns", "abc" },
+        modules = {
+            abc = function()
+                return "hi"
+            end,
+        },
+    },
+}
+
+M.nvdash = {
+    load_on_startup = true,
+
+    header = {
+        "                            ",
+        "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+        "   ▄▀███▄     ▄██ █████▀    ",
+        "   ██▄▀███▄   ███           ",
+        "   ███  ▀███▄ ███           ",
+        "   ███    ▀██ ███           ",
+        "   ███      ▀ ███           ",
+        "   ▀██ █████▄▀█▀▄██████▄    ",
+        "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+        "                            ",
+        "     Powered By eovim    ",
+        "                            ",
+    },
+
+    buttons = {
+        { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+        { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+        { txt = "  Themes", keys = "Spc t h", cmd = "Telescope themes" },
+        { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
+        -- more... check nvconfig.lua file for full list of buttons
+    },
+}
+
+M.colorify = {
+    enabled = true,
+    mode = "virtual",
+    virt_text = "󱓻 ",
+    highlight = { hex = true, lspvars = true },
 }
 
 return M
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,7 +2,5 @@ require "nvchad.options"
 
 -- add yours here!
 
-local o = vim.o
-o.cursorlineopt = "both" -- to enable cursorline!
-
-vim.wo.relativenumber = true
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
